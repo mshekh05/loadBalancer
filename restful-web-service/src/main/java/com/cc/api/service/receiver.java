3b96b8634bf84dfbeaba2f9931fb05e4b9210882
vaishee90 <aardhana@asu.edu>
2018-03-27 21:35:59 -0700

    cleaned code

--- restful-web-service/src/main/java/com/cc/api/service/receiver.java
+++ restful-web-service/src/main/java/com/cc/api/service/receiver.java
@@ -1,30 +1,17 @@
 package com.cc.api.service;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
-import com.amazonaws.services.ec2.model.DescribeInstancesResult;
-import com.amazonaws.services.ec2.model.Instance;
-import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.AccessControlList;
-import com.amazonaws.services.s3.model.GroupGrantee;
-import com.amazonaws.services.s3.model.Owner;
-import com.amazonaws.services.s3.model.Permission;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
@@ -44,43 +31,22 @@ public class receiver {
 		final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
 		String Ins_output = "";
 		String s = "";
-		String output = "";
 		
 		try {
-			System.out.println("Terminating Instance......... ");
-			//test
+			
+			// command to get current Instance ID
 			Process t = Runtime.getRuntime().exec(command_getInstanceId);
 			t.waitFor();
 			BufferedReader br = new BufferedReader(new InputStreamReader(t.getInputStream()));
 			while ((s = br.readLine()) != null) {
 				Ins_output = s;
-        			System.out.println("InstanceID: "+ Ins_output);
 			}
 			t.destroy();
 			
+			// terminate current instance from self
 			TerminateInstancesRequest tir = new TerminateInstancesRequest().withInstanceIds(Ins_output);
 			ec2.terminateInstances(tir);
-			System.out.println("Terminate instances request has been run");
-			
-			DescribeInstancesResult describeInstancesResult = ec2.describeInstances();
-            	List<Reservation> reservations = describeInstancesResult.getReservations();                
-            	List<Instance> listOfInstances = new ArrayList<>();
-            
-            System.out.println("Fetching instance ID from reservations");
-            
-            for (Reservation reservation : reservations)
-                listOfInstances.addAll(reservation.getInstances());                
-            for (Instance instance : listOfInstances) {
-            		if(instance.getInstanceId().equals(output)) {
-            			if (instance.getState().getName().equals("shutting-down")
-	                            || instance.getState().getName().equals("terminated"))
-	                        System.out.println("Termination successful.. YAYYY!! :) :)");
-            			break;
-            		}
-            }
 		} catch (Exception e) {
-			System.out.println("**************************");
-			System.out.println("Exception in terminating instance");
 			e.printStackTrace();
 		}
 	}
@@ -123,100 +89,62 @@ public class receiver {
 
 		while (true) {
 			
-			//try was here previously
-			
 			List<Message> messages = sqs.receiveMessage(requestQueueUrl).getMessages();
+			
+			// if no message received, terminate the instance
 			if(messages.isEmpty()) {
 				terminateInstance();
 			}
-				
+			
+			// do below for each message received from request Q
 			for (Message m : messages) {
 				
-				long startTime = (new Date()).getTime();
-				
-//				count = 0;
 				Url = m.getBody().toString();
 				
-				System.out.println(Url);
-				
+				// command to run image recognition
 				String command_img_recog[] = { "/bin/bash", "-c", cmd1 + cmd2 + cmd3 + Url + cmd4};
 				
 				// App Instance to run image recognition script and return output
 				try {
-					
-				    System.out.println("Inside try block");
 		            Process p = Runtime.getRuntime().exec(command_img_recog);
 		            p.waitFor();
-		            
 		            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		            System.out.println("-------------------------------------------------------------------------");
-		            
 		            while ((s = br.readLine()) != null) {
 		            		output = s;
-		            		System.out.println("Message: "+ output);
 		            }
-		            
 		            p.destroy();
 		        } catch (Exception e) {
-		        		System.out.println("**************************");
-					System.out.println("Exception in getting output from image recognition module");
-		        		System.out.println(e);
 		        		terminateInstance();
 		        }
-
-				System.out.println("Msgs worked on: "+ Url);
-
-				System.out.println("Output retrieved: " + output);
 				
+				// get image name
 				s3ProcessedUrl = Url.split("\\/");
-				
-				String[] imageRecognitionOutputArray = output.split("\\(");
-				for(String str : imageRecognitionOutputArray)
-					System.out.println("Output in array: " + str);
+
+				// get output without score value
 				imageRecognitionOutput = output.split("\\(")[0];
-				System.out.println("imageRecognitionOutput: " + imageRecognitionOutput);
 				
+				// put [imageID, output] into S3
 				try {
-					// put [imageID, output] into S3
 					s3.putObject(bucket_name, s3ProcessedUrl[s3ProcessedUrl.length - 1], imageRecognitionOutput);
-					System.out.println("Object placed in bucket");
 				} catch (Exception e) {
-					System.out.println("**************************");
-					System.out.println("Exception in S3 bucket");
-					e.printStackTrace();
 					terminateInstance();
 				}
 				
+				// delete message from request Q
 				try {
-					// delete message from request Q
 					sqs.deleteMessage(requestQueueUrl, m.getReceiptHandle());
-					System.out.println("Message deleted from request Q");
 				} catch (Exception e){
-					System.out.println("**************************");
-					System.out.println("Exception in deleting from request Q");
-					e.printStackTrace();
 					terminateInstance();
 				}
 				
-				
-				long endTime = (new Date()).getTime();	
-				long difference = endTime - startTime;
-				
-				// time taken to complete 1 request
-				System.out.println("Time taken to complete 1 request: " + difference);
-				
+				// publish message to response Q
 				try {
-					// publish message to response Q
 					SendMessageRequest send_msg_request = new SendMessageRequest()
 							.withQueueUrl(responseQueueUrl)
 							.withMessageBody(s3ProcessedUrl[s3ProcessedUrl.length - 1] + "||" + imageRecognitionOutput);
 					
 					sqs.sendMessage(send_msg_request);
-					System.out.println("Message sent to response Q");
 				} catch (Exception e) {
-					System.out.println("**************************");
-					System.out.println("Exception in sending to response Q");
-					e.printStackTrace();
 					terminateInstance();
 				}	
 			}
